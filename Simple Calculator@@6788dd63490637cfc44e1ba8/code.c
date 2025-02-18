626e0f406e7272ad140507f61025777f4ba0f22b
Bhoomi Mehta <mehtabhoomi292@gmail.com>
2025-02-18 06:32:51 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char ch; int a,b;
+    scanf("%d %d %c",&a,&b,&ch)
+    switch (ch) {
+        case '+':
+        printf("%d + %d = %d\n", a,b,a+b);
+        break;
+        case '-':
+        printf("%d + %d = %d\n", a,b,a-b);
+        break;
+        case '*':
+        printf("%d + %d = %d\n", a,b,a*b);
+        break;
+        case '/':
+        printf("%d + %d = %d\n", a,b,a/b);
+        break;
+        default:
+            printf("Invalid operator.\n");
+            break;
+    }
+    
+    return 0;
+}
